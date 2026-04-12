d319688530ec851a1d775c14fed95dcc906da34f
Essouiriaya <essouiri.aya@etu.uae.ac.ma>
2026-04-12 16:36:45 +0100

    Update Project

--- src/main/java/web/ListProduitServlet.java
+++ src/main/java/web/ListProduitServlet.java
@@ -1,6 +1,7 @@
 package web;
 
-import java.io. IOException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,20 +9,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao. Produit;
+import dao.Produit;
 import services.ProduitMetier;
 import services.ProduitMetierImpl;
 
 public class ListProduitServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	
-	private static final ProduitMetier metier = ProduitMetierImpl.getInstance();
+    private static final long serialVersionUID = 1L;
 
+    private static final ProduitMetier metier = ProduitMetierImpl.getInstance();
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        List<Produit> liste = metier.getAllProduits();
+        String idStr = req.getParameter("idProduit");
+
+        List<Produit> liste = new ArrayList<>();
+
+        if (idStr != null && !idStr.isEmpty()) {
+            try {
+                Long id = Long.parseLong(idStr);
+
+                Produit p = metier.getProduitById(id);
+
+                if (p != null) {
+                    liste.add(p);
+                }
+
+            } catch (NumberFormatException e) {
+                liste = metier.getAllProduits();
+            }
+        } else {
+            liste = metier.getAllProduits();
+        }
 
         req.setAttribute("listeProduits", liste);
         req.getRequestDispatcher("index.jsp").forward(req, resp);
